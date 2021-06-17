df662531ecf91dd09db8f1e2475c7f7b76cf8f1b
Alex <pavlovworld@hotmail.com>
2021-06-17 22:54:02 +0300

    Initial commit

---
+++ src/main/java/com/cars/app/carsrestapp/CarsRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.cars.app.carsrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cars/app/carsrestapp/CarsRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars.app.carsrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
